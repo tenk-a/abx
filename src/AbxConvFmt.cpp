1c587b0086b7a517dae6786a7161e9f4cd3ed333
tenk-a <tenka@6809.net>
2018-12-12 08:27:24 +0900

    @a,@Ba,@ba,@BA の扱いを変更。 ファイル属性文字をなるべく attrib 命令でのものに合わせる.

--- src/AbxConvFmt.cpp
+++ src/AbxConvFmt.cpp
@@ -458,104 +458,125 @@ void ConvFmt::strFmt(char *dst, size_t dstSz, char const* fmt, fks_stat_t const*
 	    	    break;
 
 			case 'a':
-				{
-				  #ifdef _WIN32
-				  	if (sepMode == 0 || sepMode == 1) {
-			    	    if (n < 1) n = 9; //8;
-					  	unsigned a = st->st_native_attr;
-					  	b = buf;
-						if (a & FKS_S_W32_ReadOnly) 		*b++ = 'r';	else *b++='-';
-						if (a & FKS_S_W32_Hidden) 			*b++ = 'h';	else *b++='-';
-						if (a & FKS_S_W32_System)    		*b++ = 's';	else *b++='-';
-						if (a & FKS_S_W32_Directory)		*b++ = 'd';	else *b++='-';
-						if (a & FKS_S_W32_Archive) 			*b++ = 'a';	else *b++='-';
-						if (a & FKS_S_W32_ReparsePoint) 	*b++ = 'p';	else *b++='-';
-						if (a & FKS_S_W32_Compressed)   	*b++ = 'c';	else *b++='-';
-						if (a & FKS_S_W32_NoIndexed)		*b++ = 'N';	else *b++='-';
-						if (a & FKS_S_W32_Encrypted) 		*b++ = 'e';	else *b++='-';
-						*b = 0;
-						if (n < 9)
-							buf[n] = 0;
-			    	    if (n > de-d-1)
-			    	    	n = de-d-1;
-		    	    	d += snprintf(d, de-d, "%-*s", n, buf);
-				  	} else if (sepMode == 2) {
-			    	    if (n < 1) n = 32; //8;
-					  	unsigned a = st->st_native_attr;
-						//a = FKS_S_W32ATTR(a);
-					  	b = buf;
-						if (a & FKS_S_W32_ReadOnly) 		*b++ = 'r';	else *b++='-';
-						if (a & FKS_S_W32_Hidden) 			*b++ = 'h';	else *b++='-';
-						if (a & FKS_S_W32_System)    		*b++ = 's';	else *b++='-';
-						if (a & FKS_S_W32_Volume)    		*b++ = '1';	else *b++='-';
-						if (a & FKS_S_W32_Directory)		*b++ = 'd';	else *b++='-';
-						if (a & FKS_S_W32_Archive) 			*b++ = 'a';	else *b++='-';
-						if (a & FKS_S_W32_Device) 			*b++ = 'D';	else *b++='-';
-						if (a & FKS_S_W32_Normal)   		*b++ = 'n';	else *b++='-';
-						if (a & FKS_S_W32_Temporary)   		*b++ = 't';	else *b++='-';
-						if (a & FKS_S_W32_SparseFile) 		*b++ = 'S';	else *b++='-';
-						if (a & FKS_S_W32_ReparsePoint) 	*b++ = 'p';	else *b++='-';
-						if (a & FKS_S_W32_Compressed)   	*b++ = 'c';	else *b++='-';
-						if (a & FKS_S_W32_Offline) 			*b++ = 'o';	else *b++='-';
-						if (a & FKS_S_W32_NoIndexed)		*b++ = 'N';	else *b++='-';
-						if (a & FKS_S_W32_Encrypted) 		*b++ = 'e';	else *b++='-';
-						if (a & FKS_S_W32_IntegritySystem) 	*b++ = 'I';	else *b++='-';
-						if (a & FKS_S_W32_Virtual) 			*b++ = 'V';	else *b++='-';
-						if (a & FKS_S_W32_NoScrubData)		*b++ = 'B';	else *b++='-';
-						if (a & FKS_S_W32_EA) 				*b++ = 'E';	else *b++='-';
-						if (a & FKS_S_W32_Pinned)			*b++ = 'P';	else *b++='-';
-						if (a & FKS_S_W32_Unpinned)			*b++ = 'U';	else *b++='-';
-						if (a & 0x00200000)					*b++ = '1'; else *b++='-';
-						if (a & FKS_S_W32_RecallOnDataAcs)	*b++ = 'A';	else *b++='-';
-						if (a & 0x00800000)					*b++ = '1'; else *b++='-';
-						if (a & 0x01000000)					*b++ = '1'; else *b++='-';
-						if (a & FKS_S_W32_StrictlySequential) *b++='Q';	else *b++='-';
-						if (a & 0x04000000)					*b++ = '1'; else *b++='-';
-						if (a & 0x08000000)					*b++ = '1'; else *b++='-';
-						if (a & 0x10000000)					*b++ = '1'; else *b++='-';
-						if (a & 0x20000000)					*b++ = '1'; else *b++='-';
-						if (a & 0x40000000)					*b++ = '1'; else *b++='-';
-						if (a & 0x80000000)					*b++ = '1'; else *b++='-';
-						*b = 0;
-						if (n < 32)
-							buf[n] = 0;
-			    	    if (n > de-d-1)
-			    	    	n = de-d-1;
-		    	    	d += snprintf(d, de-d, "%-*s", n, buf);
-		    	    } else
-				  #endif
-		    	    {
-			    	    if (n < 1) n = 10;
-					  	unsigned a = st->st_mode;
-					  	b = buf;
+			  #ifdef _WIN32
+			  	if (sepMode == 0 || sepMode == 2) {
+		    	    if (n < 1) n = 14; //9;
+				  	unsigned a = st->st_native_attr;
+				  	b = buf;
+				 #if 1
+					if (a & FKS_S_W32_ReadOnly) 		*b++ = 'r';	else *b++='-';
+					if (a & FKS_S_W32_Hidden) 			*b++ = 'h';	else *b++='-';
+					if (a & FKS_S_W32_System)    		*b++ = 's';	else *b++='-';
+					if (a & FKS_S_W32_Directory)		*b++ = 'd';	else *b++='-';
+					if (a & FKS_S_W32_Archive) 			*b++ = 'a';	else *b++='-';
+					//if (a & FKS_S_W32_SparseFile)		*b++ = 'P';	else *b++='-';
+					if (a & FKS_S_W32_ReparsePoint) 	*b++ = 'l';	else *b++='-';
+					if (a & FKS_S_W32_Compressed)   	*b++ = 'c';	else *b++='-';
+					if (a & FKS_S_W32_Offline) 			*b++ = 'o';	else *b++='-';
+					if (a & FKS_S_W32_NoIndexed)		*b++ = 'i';	else *b++='-';
+					if (a & FKS_S_W32_Encrypted) 		*b++ = 'e';	else *b++='-';
+					if (a & FKS_S_W32_IntegritySystem) 	*b++ = 'v';	else *b++='-';
+					if (a & FKS_S_W32_NoScrubData)		*b++ = 'x';	else *b++='-';
+					if (a & FKS_S_W32_Pinned)			*b++ = 'p';	else *b++='-';
+					if (a & FKS_S_W32_Unpinned)			*b++ = 'u';	else *b++='-';
+				 #else
+					if (a & FKS_S_W32_ReadOnly) 		*b++ = 'r';	else *b++='-';
+					if (a & FKS_S_W32_Hidden) 			*b++ = 'h';	else *b++='-';
+					if (a & FKS_S_W32_System)    		*b++ = 's';	else *b++='-';
+					if (a & FKS_S_W32_Directory)		*b++ = 'd';	else *b++='-';
+					if (a & FKS_S_W32_Archive) 			*b++ = 'a';	else *b++='-';
+					if (a & FKS_S_W32_ReparsePoint) 	*b++ = 'l';	else *b++='-';
+					if (a & FKS_S_W32_Compressed)   	*b++ = 'c';	else *b++='-';
+					if (a & FKS_S_W32_NoIndexed)		*b++ = 'i';	else *b++='-';
+					if (a & FKS_S_W32_Encrypted) 		*b++ = 'e';	else *b++='-';
+				 #endif
+					*b = 0;
+					if (n < 9)
+						buf[n] = 0;
+		    	    if (n > de-d-1)
+		    	    	n = de-d-1;
+	    	    	d += snprintf(d, de-d, "%-*s", n, buf);
+	    	    } else
+			  #endif
+	    	    {
+		    	    if (n < 1) n = 10;
+				  	unsigned a = st->st_mode;
+				  	b = buf;
 
-						if ((a & FKS_S_IFMT) == FKS_S_IFLNK)		*b++ = 'l';
-						else if ((a & FKS_S_IFMT) == FKS_S_IFDIR)	*b++ = 'd';
-						else 										*b++ = '-';
-						if (a & (1 << 8))			*b++ = 'r'; else *b++ = '-';
-						if (a & (1 << 7))			*b++ = 'w'; else *b++ = '-';
-						if (a & (1 << 6))			*b++ = 'x'; else *b++ = '-';
-						if (a & (1 << 5))			*b++ = 'r'; else *b++ = '-';
-						if (a & (1 << 4))			*b++ = 'w'; else *b++ = '-';
-						if (a & (1 << 3))			*b++ = 'x'; else *b++ = '-';
-						if (a & (1 << 2))			*b++ = 'r'; else *b++ = '-';
-						if (a & (1 << 1))			*b++ = 'w'; else *b++ = '-';
-						if (a & (1 << 0))			*b++ = 'x'; else *b++ = '-';
+					if ((a & FKS_S_IFMT) == FKS_S_IFLNK)		*b++ = 'l';
+					else if ((a & FKS_S_IFMT) == FKS_S_IFDIR)	*b++ = 'd';
+					else 										*b++ = '-';
+					if (a & (1 << 8))			*b++ = 'r'; else *b++ = '-';
+					if (a & (1 << 7))			*b++ = 'w'; else *b++ = '-';
+					if (a & (1 << 6))			*b++ = 'x'; else *b++ = '-';
+					if (a & (1 << 5))			*b++ = 'r'; else *b++ = '-';
+					if (a & (1 << 4))			*b++ = 'w'; else *b++ = '-';
+					if (a & (1 << 3))			*b++ = 'x'; else *b++ = '-';
+					if (a & (1 << 2))			*b++ = 'r'; else *b++ = '-';
+					if (a & (1 << 1))			*b++ = 'w'; else *b++ = '-';
+					if (a & (1 << 0))			*b++ = 'x'; else *b++ = '-';
 
-						*b = 0;
-			    	    if (n > de-d-1)
-			    	    	n = de-d-1;
-		    	    	d += snprintf(d, de-d, "%-*s", n, buf);
-					}
+					*b = 0;
+		    	    if (n > de-d-1)
+		    	    	n = de-d-1;
+	    	    	d += snprintf(d, de-d, "%-*s", n, buf);
 				}
 				break;
 
 			case 'A':
-				if (n < 0)
-					n = 4;
-	    	    if (n > de-d-1)
-	    	    	n = de-d-1;
-				d += snprintf(d, de-d, "%0*x", n, st->st_native_attr);
+			  #ifdef _WIN32
+			  	if (sepMode == 2) {
+		    	    if (n < 1) n = 32; //8;
+				  	unsigned a = st->st_native_attr;
+					//a = FKS_S_W32ATTR(a);
+				  	b = buf;
+					if (a & FKS_S_W32_ReadOnly) 		*b++ = 'r';	else *b++='-';
+					if (a & FKS_S_W32_Hidden) 			*b++ = 'h';	else *b++='-';
+					if (a & FKS_S_W32_System)    		*b++ = 's';	else *b++='-';
+					if (a & FKS_S_W32_Volume)    		*b++ = '1';	else *b++='-';
+					if (a & FKS_S_W32_Directory)		*b++ = 'd';	else *b++='-';
+					if (a & FKS_S_W32_Archive) 			*b++ = 'a';	else *b++='-';
+					if (a & FKS_S_W32_Device) 			*b++ = 'D';	else *b++='-';
+					if (a & FKS_S_W32_Normal)   		*b++ = 'n';	else *b++='-';
+					if (a & FKS_S_W32_Temporary)   		*b++ = 't';	else *b++='-';
+					if (a & FKS_S_W32_SparseFile) 		*b++ = 'p';	else *b++='-';
+					if (a & FKS_S_W32_ReparsePoint) 	*b++ = 'l';	else *b++='-';
+					if (a & FKS_S_W32_Compressed)   	*b++ = 'c';	else *b++='-';
+					if (a & FKS_S_W32_Offline) 			*b++ = 'o';	else *b++='-';
+					if (a & FKS_S_W32_NoIndexed)		*b++ = 'i';	else *b++='-';
+					if (a & FKS_S_W32_Encrypted) 		*b++ = 'e';	else *b++='-';
+					if (a & FKS_S_W32_IntegritySystem) 	*b++ = 'v';	else *b++='-';
+					if (a & FKS_S_W32_Virtual) 			*b++ = 'V';	else *b++='-';
+					if (a & FKS_S_W32_NoScrubData)		*b++ = 'x';	else *b++='-';
+					if (a & FKS_S_W32_EA) 				*b++ = 'E';	else *b++='-';
+					if (a & FKS_S_W32_Pinned)			*b++ = 'P';	else *b++='-';
+					if (a & FKS_S_W32_Unpinned)			*b++ = 'U';	else *b++='-';
+					if (a & 0x00200000)					*b++ = '1'; else *b++='-';
+					if (a & FKS_S_W32_RecallOnDataAcs)	*b++ = 'A';	else *b++='-';
+					if (a & 0x00800000)					*b++ = '1'; else *b++='-';
+					if (a & 0x01000000)					*b++ = '1'; else *b++='-';
+					if (a & FKS_S_W32_StrictlySequential) *b++='Q';	else *b++='-';
+					if (a & 0x04000000)					*b++ = '1'; else *b++='-';
+					if (a & 0x08000000)					*b++ = '1'; else *b++='-';
+					if (a & 0x10000000)					*b++ = '1'; else *b++='-';
+					if (a & 0x20000000)					*b++ = '1'; else *b++='-';
+					if (a & 0x40000000)					*b++ = '1'; else *b++='-';
+					if (a & 0x80000000)					*b++ = '1'; else *b++='-';
+					*b = 0;
+					if (n < 32)
+						buf[n] = 0;
+		    	    if (n > de-d-1)
+		    	    	n = de-d-1;
+	    	    	d += snprintf(d, de-d, "%-*s", n, buf);
+	    	    } else
+			  #endif
+	    	    {
+					if (n < 0)
+						n = 4;
+		    	    if (n > de-d-1)
+		    	    	n = de-d-1;
+					d += snprintf(d, de-d, "%0*x", n, st->st_native_attr);
+				}
 				break;
 
 	    	default:
--- src/AbxFiles.hpp
+++ src/AbxFiles.hpp
@@ -15,33 +15,6 @@
 
 typedef std::vector<struct Fks_DirEntPathStat const*>	PathStats;
 
-/*
-enum FileAttr {
-    FA_RdOnly 	= 0x01,		//FILE_ATTRIBUTE_READONLY,
-    FA_Hidden 	= 0x02, 	//FILE_ATTRIBUTE_HIDDEN,
-    FA_Sys    	= 0x04, 	//FILE_ATTRIBUTE_SYSTEM,
-    //FA_Volume = 0x08,		// DOS FAT
-    FA_Dir    	= 0x10, 	//FILE_ATTRIBUTE_DIRECTORY,
-    FA_Arcive 	= 0x20, 	//FILE_ATTRIBUTE_ARCHIVE,
-	//FA_Device = 0x40, 	//FILE_ATTRIBUTE_DEVICE,
-	FA_Norm   	= 0x80, 	//FILE_ATTRIBUTE_NORMAL,
-	FA_Temp	  	= 0x100,	// FILE_ATTRIBUTE_TEMPORARY,
-	//FA_Sparse = 0x200, 	// FILE_ATTRIBUTE_SPARSE_FILE
-	//FA_Reparse= 0x400, 	// FILE_ATTRIBUTE_SPARSE_FILE
-	FA_Comp	  	= 0x800, 	// FILE_ATTRIBUTE_COMPRESSED,
-	//FA_Offline= 0x1000,	// FILE_ATTRIBUTE_OFFLINE
-	//FA_No_indexed=0x2000,	// FILE_ATTRIBUTE_NOT_CONTENT_INDEXED
-    FA_Encrypt	= 0x4000,	//FILE_ATTRIBUTE_ENCRYPTED,
-	//FA_IntegritySys=0x8000,	// FILE_ATTRIBUTE_INTEGRITY_STREAM
-	FA_Virtual	= 0x10000,		// FILE_ATTRIBUTE_VIRTUAL
-	//FA_NoScrub	= 0x20000,	// FILE_ATTRIBUTE_NO_SCRUB_DATA
-	//FA_EA		= 0x40000,	// FILE_ATTRIBUTE_NO_SCRUB_DATA
-	//FA_MASK_NOARC = FA_Norm|FA_RdOnly|FA_Hidden|FA_Sys|FA_Volume|FA_Dir,
-	FA_MASK_FILEYTPE = FA_RdOnly|FA_Hidden|FA_Sys|FA_Norm|FA_Arcive,
-	FA_MASK_NOARC = FA_RdOnly|FA_Hidden|FA_Sys|FA_Dir|FA_Norm|FA_Temp|FA_Comp|FA_Encrypt|FA_Virtual,
-	FA_MASK   = FA_MASK_NOARC | FA_Arcive,
-};
-*/
 
 enum SearchAttrType {
 	SRCH_DIR	= FKS_DE_Dir,
